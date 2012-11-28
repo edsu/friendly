22590573e9324dc5f3df09f10e68d3ef1f852412
Ed Summers <ehs@pobox.com>
2012-11-28 05:57:06 -0500

    cajoling the todo app into friendly while listening to #swib12 livestream :-)

--- public/js/app.js
+++ public/js/app.js
@@ -1,82 +1,24 @@
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
 
-  // Todo Model
-  // ----------
-
-  // Our basic **Todo** model has `content`, `order`, and `done` attributes.
-  var Todo = Backbone.Model.extend({
-
-    // Default attributes for the todo.
+  // Models
+  
+  var Website = Backbone.Model.extend({
     defaults: {
-      content: "empty todo...",
-      done: false
-    },
-
-    // Ensure that each todo created has `content`.
-    initialize: function() {
-      if (!this.get("content")) {
-        this.set({"content": this.defaults.content});
-      }
-    },
-
-    // Toggle the `done` state of this todo item.
-    toggle: function() {
-      this.save({done: !this.get("done")});
-    },
-
-    // Remove this Todo from *localStorage* and delete its view.
-    clear: function() {
-      this.destroy();
+      url: null,
+      title: null
     }
-
   });
 
-  // Todo Collection
-  // ---------------
-
-  // The collection of todos is backed by *localStorage* instead of a remote
-  // server.
-  var TodoList = Backbone.Collection.extend({
-
-    // Reference to this collection's model.
-    model: Todo,
-
-    // Save all of the todo items under the `"todos"` namespace.
-    localStorage: new Store("todos-backbone"),
-
-    // Filter down the list of all todo items that are finished.
-    done: function() {
-      return this.filter(function(todo){ return todo.get('done'); });
-    },
-
-    // Filter down the list to only todo items that are still not finished.
-    remaining: function() {
-      return this.without.apply(this, this.done());
-    },
-
-    // We keep the Todos in sequential order, despite being saved by unordered
-    // GUID in the database. This generates the next order number for new items.
-    nextOrder: function() {
-      if (!this.length) return 1;
-      return this.last().get('order') + 1;
-    },
-
-    // Todos are sorted by their original insertion order.
-    comparator: function(todo) {
-      return todo.get('order');
-    }
-
+  var WebsiteList = Backbone.Collection.extend({
+    model: Website,
   });
 
-  // Create our global collection of **Todos**.
-  var Todos = new TodoList;
+  var websites  = new WebsiteList;
 
-  // Todo Item View
-  // --------------
-
-  // The DOM element for a todo item...
-  var TodoView = Backbone.View.extend({
+  // Views
+  
+  var WebsiteView = Backbone.View.extend({
 
     //... is a list tag.
     tagName:  "li",
